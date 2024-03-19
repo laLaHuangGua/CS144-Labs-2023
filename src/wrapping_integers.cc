ce8dd9402dc9f0717280ca838ae22a8ba91d918b
laLaHuangGua <1270562182@qq.com>
2024-03-19 22:41:52 +0800

    C2 R1: minor change to Wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,19 +4,18 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return zero_point + n;
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  const uint64_t base = ( ( checkpoint >> 32 ) << 32 );
+  uint64_t val = ( ( checkpoint >> 32 ) << 32 );
   const int64_t offset = raw_value_ - zero_point.raw_value_;
-  uint64_t val = base + offset;
-  if ( base != 0 && offset > 0 ) {
-    val -= ( 1UL << 32 );
-  };
-  val = min( { val, val + ( 1UL << 32 ), val + ( 1UL << 33 ) }, [checkpoint]( const uint64_t a, const uint64_t b ) {
-    return DIST( a, checkpoint ) < DIST( b, checkpoint );
-  } );
+  if ( offset < 0 ) {
+    val += ( 1UL << 32 );
+  }
+  val += offset;
+  val = min( { val - ( 1UL << 32 ), val, val + ( 1UL << 32 ) },
+             [checkpoint]( uint64_t a, uint64_t b ) { return DIST( a, checkpoint ) < DIST( b, checkpoint ); } );
   return val;
 }
